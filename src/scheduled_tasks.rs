2c183358d380bdb5834ec35b6073f5ac890cc7e9
lazypassion <25536767+lazypassion@users.noreply.github.com>
2025-12-28 23:26:36 -0500

    update scheduled task future to handle cancellation

--- src/scheduled_tasks.rs
+++ src/scheduled_tasks.rs
@@ -26,12 +26,14 @@ pub async fn handle_scheduled_tasks(
     let mut signal_close = windows::ctrl_close().expect("ctrl close to be available");
     let mut signal_shutdown = windows::ctrl_shutdown().expect("ctrl shutdown to be available");
 
+    let batch_tracker = TaskTracker::new();
     loop {
         let signal_tracker_handle = main_tracker.clone();
         tokio::select! {
             biased;
             _ = cancellation_token.cancelled() => {
-                info!("scheduled tasks received task cancelled");
+                warn!("scheduled tasks received task cancelled");
+                batch_tracker.wait().await;
                 break;
             },
             _ = signal_c.recv() => {
@@ -54,10 +56,17 @@ pub async fn handle_scheduled_tasks(
                 cancellation_token.cancel();
                 signal_tracker_handle.close();
             },
-            _ = schedule_tasks(task_count, pool.clone(), cancellation_token.clone()) => {
-                let sleep_length = Duration::from_secs(30);
-                info!("All scheduled tasks complete, sleeping for {:?}", sleep_length);
-                time::sleep(sleep_length).await;
+            result = schedule_tasks(task_count, pool.clone(), batch_tracker.clone(), cancellation_token.clone()) => {
+                let (batch, ready_scheduled_tasks_count) = result.unwrap();
+                batch.wait().await;
+                // this runs if the batch is empty which is always the case if I wait till the tasks finish
+                // need to a way to decide that I should keep looping until
+                // probably need to return the pending count or something like that
+                if let None = ready_scheduled_tasks_count {
+                    let sleep_length = Duration::from_secs(10);
+                    warn!("All scheduled tasks complete, sleeping for {:?}", sleep_length);
+                    // time::sleep(sleep_length).await;
+                }
             },
         }
     }
@@ -66,8 +75,9 @@ pub async fn handle_scheduled_tasks(
 async fn schedule_tasks(
     task_count: u32,
     pool: Arc<Pool<PostgresConnectionManager<NoTls>>>,
+    batch_tracker: TaskTracker,
     cancellation_token: CancellationToken,
-) -> Result<(), WorkerError> {
+) -> Result<(TaskTracker, Option<u32>), WorkerError> {
     let statement = "WITH task AS (
     SELECT * FROM messages
     WHERE scheduled_at IS NOT NULL AND status = 'pending' AND scheduled_at <= NOW()
@@ -82,68 +92,66 @@ async fn schedule_tasks(
     let count_statement = "SELECT COUNT(*) FROM messages
         WHERE scheduled_at IS NOT NULL AND status = 'pending' AND scheduled_at <= NOW()";
 
-    loop {
-        // Check for cancellation at the start of each iteration
-        if cancellation_token.is_cancelled() {
-            info!("Cancellation requested, exiting schedule_tasks");
-            return Ok(());
-        }
-
-        let pending_scheduled_tasks_count = {
-            let connection = match pool.get().await {
-                Ok(conn) => conn,
-                Err(err) => {
-                    error!("Failed to get connection from pool: {:?}", err);
-                    return Err(WorkerError::PoolError(err));
-                }
-            };
-
-            let pending_count: i64 = match connection.query_one(count_statement, &[]).await {
-                Ok(row) => row.get(0),
-                Err(err) => {
-                    error!("Failed to query pending scheduled task count: {:?}", err);
-                    return Err(err.into());
-                }
-            };
-            pending_count
-        };
-
-        if pending_scheduled_tasks_count == 0 {
-            info!("No pending scheduled tasks found");
-            break;
-        }
-
-        info!(
-            "Found {} pending scheduled tasks, scheduling batch of {} workers",
-            pending_scheduled_tasks_count, task_count
-        );
-
-        let batch_tracker = TaskTracker::new();
-
-        // if pending tasks are less than given task count only spin up what's necessary
-        // to avoid querying the database more than necessary
-        let task_count = task_count.min(pending_scheduled_tasks_count as u32);
-
-        for _ in 0..task_count {
-            let cloned_pool = pool.clone();
-            batch_tracker.spawn(worker_run(cloned_pool, statement));
-        }
-
-        batch_tracker.close();
-
-        tokio::select! {
-            biased;
-            _ = cancellation_token.cancelled() => {
-                info!("Cancellation requested during batch execution, waiting for current batch to complete");
-                batch_tracker.wait().await;
-                info!("Batch completed after cancellation, exiting schedule_tasks");
-                return Ok(());
-            }
-            _ = batch_tracker.wait() => {
-                warn!("Batch complete, checking for remaining scheduled tasks");
-            }
-        }
+    // loop {
+    // Check for cancellation at the start of each iteration
+    if cancellation_token.is_cancelled() {
+        warn!("Cancellation requested, exiting schedule_tasks");
+        return Ok((batch_tracker, None));
     }
 
-    Ok(())
+    let pending_scheduled_tasks_count = {
+        let connection = match pool.get().await {
+            Ok(conn) => conn,
+            Err(err) => {
+                error!("Failed to get connection from pool: {:?}", err);
+                return Err(WorkerError::PoolError(err));
+            }
+        };
+
+        let pending_count: i64 = match connection.query_one(count_statement, &[]).await {
+            Ok(row) => row.get(0),
+            Err(err) => {
+                error!("Failed to query pending scheduled task count: {:?}", err);
+                return Err(err.into());
+            }
+        };
+        pending_count
+    };
+
+    if pending_scheduled_tasks_count == 0 {
+        warn!("No pending scheduled tasks found");
+        return Ok((batch_tracker, None));
+    }
+
+    warn!(
+        "Found {} pending scheduled tasks, scheduling batch of {} workers",
+        pending_scheduled_tasks_count, task_count
+    );
+
+    // if pending tasks are less than given task count only spin up what's necessary
+    // to avoid querying the database more than necessary
+    let task_count = task_count.min(pending_scheduled_tasks_count as u32);
+
+    for _ in 0..task_count {
+        let cloned_pool = pool.clone();
+        batch_tracker.spawn(worker_run(cloned_pool, statement));
+    }
+
+    batch_tracker.close();
+
+    tokio::select! {
+        biased;
+        _ = cancellation_token.cancelled() => {
+            warn!("Cancellation requested during batch execution, waiting for current batch to complete");
+            batch_tracker.wait().await;
+            warn!("Batch completed after cancellation, exiting schedule_tasks");
+            return Ok((batch_tracker, None));
+        }
+        _ = batch_tracker.wait() => {
+            warn!("Batch complete, checking for remaining scheduled tasks");
+        }
+    }
+    // }
+
+    Ok((batch_tracker, Some(pending_scheduled_tasks_count as u32)))
 }
